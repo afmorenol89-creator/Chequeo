ee60255f1ea5514a08e8450704b385a0010902aa
afmorenol89-creator <afmorenol89@gmail.com>
2026-08-31 17:54:57 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: guarda la aplicación en la tablet para que abra sin internet. */
-const CACHE = 'chequeo-v1';
+const CACHE = 'chequeo-v2';
 const ARCHIVOS = [
   './',
   './index.html',
